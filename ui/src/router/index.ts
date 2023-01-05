2d23669d3aa9165fb8d63be59f95eb0ac71952c2
oktapascal <oktaiscool@gmail.com>
2023-01-05 19:42:41 +0700

    Update index.ts
    Create redirect root to login/dashboard

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -9,6 +9,10 @@ const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: "/",
+            redirect: "/login"
+        },
+        {
             path: "/login",
             component: Login,
             meta: {
